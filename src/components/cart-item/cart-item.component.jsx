c80afc80659e07cd0660ba52b037702173471d9d
Arnaldo Corzo Ferrer <aferrercrafter@gmail.com>
2020-06-15 19:13:06 -0300

    cart item component connected to cart dropdown

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -5,11 +5,13 @@ import './cart-item.style.scss';
 const CartItem = ({ item: { imageUrl, price, name, quantity }}) => (
     <div className='cart-item'>
         <img src={imageUrl} alt='item'/>
-        <div className='itemetails'>
+        <div className='item-details'>
             <span className='name'>{name}</span>
             <span className='price'>
-                {quantity} * ${price}
+                {quantity} X ${price}
             </span>
         </div>
     </div>
 )
+
+export default CartItem;
